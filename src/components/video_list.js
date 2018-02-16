cf0fd4eeb0d074e9195ca265e2c069d3f3110fbc
barnettm <barnett717@gmail.com>
2018-02-16 13:20:09 -0800

    added click handlers

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,8 +4,14 @@ import VideoListItem from  './video_list_item';
 
 
 const VideoList = (props) => {
-    const videoItems = props.videos.map((video) => {
-        return <VideoListItem  key={video.etag} video={video} />
+    const videoItems = props.videos.map((video, onVideoSelect) => {
+        return (
+            <VideoListItem  
+                onVideoSelect={props.onVideoSelect} 
+                key={video.etag} 
+                video={video} 
+            />
+        )
     })
 
     return (
